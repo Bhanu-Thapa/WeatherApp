815510a019599b3ea678aaca3c172869cab5820e
BhanuThapa <bhanuthapa252@gmail.com>
2023-01-14 16:59:47 +0530

    convert f to c

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ var requests = require('requests');
 const homeFile = fs.readFileSync('home.html', 'utf-8');
 
 const replaceVal = (tempVal, orgVal) => {
-  let temperature = tempVal.replace('{%tempval%}', orgVal.main.temp);
-  temperature = temperature.replace('{%tempmin%}', orgVal.main.temp_min);
-  temperature = temperature.replace('{%tempmax%}', orgVal.main.temp_max);
+  let temperature = tempVal.replace('{%tempval%}', orgVal.main.temp - 273.15);
+  temperature = temperature.replace(
+    '{%tempmin%}',
+    orgVal.main.temp_min - 273.15
+  );
+  temperature = temperature.replace(
+    '{%tempmax%}',
+    orgVal.main.temp_max - 273.15
+  );
   temperature = temperature.replace('{%location%}', orgVal.name);
   temperature = temperature.replace('{%country%}', orgVal.sys.country);
   temperature = temperature.replace('{%tempstatus%}', orgVal.weather[0].main);
